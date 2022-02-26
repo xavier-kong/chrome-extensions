c60111bac235082ad74d20fcb78c838c16a8910a
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-26 17:35:56 +0800

    trying something new

--- myscript.js
+++ myscript.js
@@ -1,2 +1,20 @@
-const center = document.getElementById('center');
-center.style.display = 'none';
+let freq = 1000;
+
+const search = setInterval(() => {
+    try {
+        if (document.getAnimations('center')) {
+            document.getElementById('center').style.display = 'none';
+            freq = 100000
+            console.log('found', freq);
+        } else {
+            freq = 1000;
+            console.log('not found', freq);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}, freq)
+
+search();
+
+let found = false;
