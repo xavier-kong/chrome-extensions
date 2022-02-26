e5ffb52070fb2d16027e65ca294cf53a21f58a6d
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-26 17:48:50 +0800

    i have no idea how this works

--- myscript.js
+++ myscript.js
@@ -1,20 +1,6 @@
-let freq = 1000;
-
-const search = setInterval(() => {
-    try {
-        if (document.getAnimations('center')) {
-            document.getElementById('center').style.display = 'none';
-            freq = 100000
-            console.log('found', freq);
-        } else {
-            freq = 1000;
-            console.log('not found', freq);
-        }
-    } catch (error) {
-        console.log(error);
-    }
-}, freq)
-
-search();
-
-let found = false;
+try {
+    document.getElementById('center').style.display = 'none';
+} catch (error) {
+    console.log(error);
+    document.getElementById('center').style.display = 'none';
+}
