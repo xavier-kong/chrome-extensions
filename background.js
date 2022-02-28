e9161a13f453e2b4304714611f9b993a13250f34
xavier-kong <kongxianchengxavier@gmail.com>
2022-02-28 20:08:43 +0800

    finally making some progress with this seemingly simply extension

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-    console.log('hola', details);
+chrome.webNavigation.onHistoryStateUpdated.addListener(async (details) => {
+    console.log('hello', new Date());
+    const tab = await getCurrentTab();
+    chrome.tabs.sendMessage(tab.id, {message: 'hello'})
 })
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
--- myscript.js
+++ myscript.js
@@ -4,4 +4,10 @@ setInterval(() => {
     } catch (error) {
         console.log(error);
     }
+    console.log('active');
 }, 10000)
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log('mione', message);
+}
+)
