e34803d7214302b21a4bfa0901aaaa4473d47cff
xavier-kong <kongxianchengxavier@gmail.com>
2022-03-17 19:13:44 +0800

    added logic to get total seconds

--- yt-playlist-length/main.js
+++ yt-playlist-length/main.js
@@ -1,21 +1,31 @@
 setTimeout(() => {
-    const time = Array.from(
+    function getSecondsFromString(string) {
+        const time = string.split(':');
+        const minutes = Number(time[0]);
+        const seconds = Number(time[1]);
+        const totalSeconds = minutes * 60 + seconds;
+        return totalSeconds;
+    }
+
+    const overlayElements = Array.from(
         document.querySelectorAll(
             '.ytd-playlist-video-list-renderer .ytd-thumbnail-overlay-time-status-renderer'
         )
-    )
+    );
+    const timeStrings = overlayElements
         .filter((el) => {
             if (el.textContent) {
                 return el;
             }
         })
-        .map((el) => el.textContent.replace(/\s/g, ''))
-        .forEach((el) => {
-            /*
-            regex check for mins, secs
-            add to secs
-            */
+        .map((el) => {
+            const timeString = el.textContent.replace(/\s/g, '');
+            const seconds = getSecondsFromString(timeString);
+            return seconds;
         });
+    const seconds = timeStrings.reduce((a, b) => {
+        return a + b;
+    });
 
-    console.log(time);
+    
 }, 1000);
