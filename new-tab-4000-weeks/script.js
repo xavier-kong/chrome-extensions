b1be52c4adfff06a5e0475ce469711d7a1067013
xavier-kong <kongxianchengxavier@gmail.com>
2022-03-26 16:00:00 +0800

    working on script.js for 4000

--- new-tab-4000-weeks/script.js
+++ new-tab-4000-weeks/script.js
@@ -1,17 +1,25 @@
 let year;
 
-chrome.storage.local.get(['birthYear'], (result) => {
-    if (result) {
-        const birthYear = result['birthYear'];
-        year = birthYear;
+try {
+    chrome.storage.local.get(['birthYear'], (result) => {
+        if (result) {
+            const birthYear = result['birthYear'];
+            year = birthYear;
 
-        const promptDiv = document.getElementById('prompt');
-        promptDiv.style.display = 'none';
-    }
-});
+            hidePrompt();
+        }
+    });
+} catch (error) {}
 
 function isValidInput(input) {
-    // return true if number is a valid year
+    const year = parseInt(input);
+    if (input.length === 4 && year) {
+        // the oldest living person in world Ms. Kane Tanaka was born in 1903
+        if (year < new Date().getFullYear && year >= 1903) {
+            return year;
+        }
+    }
+    return false;
 }
 
 const generateError = () => {
@@ -22,19 +30,38 @@ const generateError = () => {
     }, 3500);
 };
 
+function hidePrompt() {
+    const promptDiv = document.getElementById('prompt');
+    promptDiv.style.display = 'none';
+}
+
+function calculateWeeks(year) {
+    const currentYear = new Date().getFullYear;
+    const yearsLeft = 88 - currentYear;
+    const weeksLeft = 52 * yearsLeft;
+    return weeksLeft;
+}
+
+function generateVisual() {
+    const weeks = calculateWeeks(year);
+}
+
 const form = document.getElementById('form');
-form.onsubmit = (e) => {
+form.onsubmit = async (e) => {
     e.preventDefault();
-    const userInput = document.getElementById('input').value;
-    if (isValidInput(userInput)) {
+    const userInput = isValidInput(document.getElementById('input').value);
+    form.reset();
+    if (userInput) {
         await chrome.storage.local.set({
             birthYear: userInput,
         });
+        year = userInput;
+        hidePrompt();
+
+        const displayDiv = document.getElementById('display');
+        displayDiv.style.display = 'block';
+        generateVisual();
     } else {
         generateError();
     }
 };
-
-// set value to browser
-// set global value
-// prompt display none, diplay display block
