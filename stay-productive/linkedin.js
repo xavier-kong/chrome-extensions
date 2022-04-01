a56a8d9509a10bb8394dadba3aea79860c3552cd
xavier-kong <kongxianchengxavier@gmail.com>
2022-04-01 18:00:15 +0800

    added logic to hide posts

---
+++ stay-productive/linkedin.js
@@ -0,0 +1,37 @@
+const targetPhrases = [
+    'Promoted',
+    'likes this',
+    'commented on this',
+    'celebrates this',
+    'loves this',
+    'like this',
+    'Add to your feed',
+    'supports this',
+    'Events recommended for you',
+    'Jobs recommended for you',
+];
+
+function findPosts() {
+    const posts = document.querySelectorAll('.feed-shared-update-v2');
+    const postsArr = Array.from(posts);
+    const badPosts = postsArr.filter(
+        (post) =>
+            targetPhrases.some((phrase) => post.innerText.includes(phrase)) &&
+            post.style.display !== 'none'
+    );
+    return badPosts;
+}
+
+function hidePosts(posts) {
+    posts.forEach((post) => {
+        post.style.display = 'none';
+    });
+}
+
+window.onscroll = (e) => {
+    const posts = findPosts();
+    hidePosts(posts);
+};
+
+const posts = findPosts();
+hidePosts(posts);
