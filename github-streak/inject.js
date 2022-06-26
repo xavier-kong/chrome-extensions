32184a7a7fc11caf1c007066f34375abbcc65ac9
xavier-kong <kongxianchengxavier@gmail.com>
2022-06-26 19:03:35 +0800

    One year, 365 days of coding. What shocked me the most was not that I learnt to code and got a software engineer job in a year, but that I was able to continue doing something without fail for a year. While I did not invent the next facebook while I was programming, I did learn to persevere. It could've been my ego reminding me of the shame of failure if I failed to commit for the day, but nevertheless, I persevered. And at the end of the day, that is what means the most to me. Life is too short for me to not aim higher and higher. See you at the top.

--- github-streak/inject.js
+++ github-streak/inject.js
@@ -1,11 +1,39 @@
 /*
-0. get username
-    how to tell if user name?
-1. fetch the data
-2. parse the result to get the data we need
-3. generate correct css and html
-4. append to correct node
+1. if no start date in storage, ask user to add streak start date
+2. find latest contribution and create contribution in last year, curr streak and longest streak
+3. if see gap in user streak, update streak start date and longest streak info
+4. display date
 */
+if (checkIfOnProfile()) {
+    const username = getUsername();
+    const startDate = getStartDateIfExists(username);
+    if (startDate) {
+        /*
+        get contributions in last year from html
+        traverse the graph to find day where commits = 0
+        if find most recent empty day:
+            if none found:
+                streak = curr day - start day
+                update longest streak
+            if earlier than today:
+                update start day
+                update longest streak
+                streak = curr day - start day
+            if today is empty:
+                streak = curr day - start day - 1
+        */
+    } else {
+        // get user to enter data do this later
+
+        chrome.storage.local.set({
+            'github-streak': {
+                'xavier-kong': {
+                    startDate: '2021-06-27',
+                },
+            },
+        });
+    }
+}
 
 function checkIfOnProfile() {
     const el = document.getElementsByClassName('js-yearly-contributions');
@@ -18,28 +46,52 @@ function getUsername() {
     return username;
 }
 
-function fetchData(username) {
-    const myInit = {
-        mode: 'no-cors',
-        method: 'GET',
-    };
-
-    fetch(
-        `https://github-readme-streak-stats.herokuapp.com/?user=${username}`,
-        myInit
-    )
-        .then((res) => res.text())
-        .then((str) => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(str, 'image/svg+xml');
-            return doc;
-        })
-        .then((doc) => {
-            console.log(doc);
-        });
+function getStartDateIfExists(username) {
+    let startDate = false;
+    chrome.storage.local.get(['github-streak'], (result) => {
+        if (result && 'github-streak' in result) {
+            if (username in result['github-streak']) {
+                startDate = result['github-streak'][username].startDate;
+            }
+        }
+    });
+    return startDate;
 }
 
-if (checkIfOnProfile()) {
-    const username = getUsername();
-    fetchData(username);
+function fetchContributionGraphArray(username) {
+    const graphJSON = fetch(
+        `https://github.com/users/${username}/contributions`
+    )
+        .then((res) => res.text())
+        .then((graph) => convertGraphToJson(graph))
+        .catch((e) => {
+            console.log(e);
+            return false;
+        });
+    return graphJSON;
+}
+
+function convertGraphToArray(graph) {
+    let data;
+    let re = /(data-count="\d+".*data-date="\d{4}-\d{2}-\d{2}")/g;
+    let matches = graph.match(re);
+    data = matches.map((match) => {
+        return {
+            count: +match.match(/data-count="(\d+)"/)[1],
+            date: match.match(/data-date="(\d{4}-\d{2}-\d{2})"/)[1],
+        };
+    });
+    return data;
+}
+
+function createStartDateForm() {}
+
+function injectStartDateForm(startDateForm) {
+    const graph = document.getElementsByClassName(
+        'graph-before-activity-overview'
+    )[0];
+    const formContainer = document.createElement('div');
+    formContainer.classList.add('start-date-form');
+    formContainer.innerHTML = startDateForm;
+    graph.appendChild(formContainer);
 }
