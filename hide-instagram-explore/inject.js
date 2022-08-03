c369ffe8d53d515362892d501845469c789237d7
xavier-kong <kongxianchengxavier@gmail.com>
2022-08-03 21:45:03 +0800

    ok now this is getting out of hand

--- hide-instagram-explore/inject.js
+++ hide-instagram-explore/inject.js
@@ -1,15 +1,15 @@
-
-chrome.webNavigation.onCompleted.addListener(() => {
-    const observer = new MutationObserver(() => {
-        const exploreButton = document.querySelectorAll("a[href='/explore/']");
-        console.log(exploreButton)
-        if (exploreButton && exploreButton[0]) {
-            console.log(exploreButton[0])
-
+const observer = new MutationObserver(() => {
+    const exploreButton = document.querySelectorAll("a[href='/explore/']");
+    if (exploreButton && exploreButton[0]) {
+        exploreButton[0].style = {
+            display: 'none'
         }
-    });
 
-    const config = { attributes: true, childList: true, subtree: true };
+    }
+});
 
+const config = { attributes: true, childList: true, subtree: true };
+
+window.onload = function() {
     observer.observe(document, config);
-})
+}
