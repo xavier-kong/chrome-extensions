6bd4f766676745a4dceede251ba43bdf38c8922c
xavier-kong <kongxianchengxavier@gmail.com>
2023-02-25 19:04:23 +0800

    added hide-youtube-search

---
+++ hide-youtube-search/main.js
@@ -0,0 +1,13 @@
+
+const searchBar = document.getElementsByTagName("ytd-searchbox");
+
+searchBar[0].style.display = 'none';
+
+const voiceSearchButton = document.getElementById("voice-search-button");
+
+voiceSearchButton.style.display = 'none';
+
+const logo = document.getElementById("logo");
+
+logo.style.display = 'none';
+
