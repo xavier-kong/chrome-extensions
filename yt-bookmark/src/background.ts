e5e78041be31cd07c5c47d5673bd8f03144cf40b
xavier-kong <kongxianchengxavier@gmail.com>
2024-01-09 21:01:49 +0800

    too many injection

--- yt-bookmark/src/background.ts
+++ yt-bookmark/src/background.ts
@@ -1,11 +1,20 @@
+const executedMap: Record<number, boolean> = {};
+
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (
-        tab?.url?.includes('https://www.youtube.com/watch?v=') ||
-        changeInfo?.url?.includes('https://www.youtube.com/watch?v=')
-    ) {
+    console.log(tabId in executedMap);
+    if (tab?.url?.includes('https://www.youtube.com/watch?v=') && !(tabId in executedMap)) {
+
         chrome.scripting.executeScript({
             target: { tabId: tabId },
             files: ['./main.js'],
         });
+
+        executedMap[tabId] = true;
+    }
+})
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+    if (tabId in executedMap) {
+        delete executedMap[tabId];
     }
 })
