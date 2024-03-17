3f3295b9142b63bb35bb8a05088d0af73c418d28
xavier-kong <kongxianchengxavier@gmail.com>
2024-03-17 14:51:47 +0800

    cross platform

---
+++ no-english-yt/background.js
@@ -0,0 +1,8 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab?.url?.includes('https://www.youtube.com/watch?v=')) {
+        chrome.scripting.executeScript({
+            target: { tabId },
+            files: ['./inject.js'],
+        });
+    }
+})
